287f3d1aff36488cd6ca82634d3ecd51ca3fc5d6
Iris25-dev <ronaldoiris725@gmail.com>
2025-03-29 11:48:55 +0000

    Update Config

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    domains: ['localhost'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader']
+    });
+    return config;
+  },
+  transpilePackages: ['@solana/wallet-adapter-react-ui']
+}
+
+export default nextConfig
